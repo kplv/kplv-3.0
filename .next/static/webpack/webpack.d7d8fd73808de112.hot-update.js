c159713bbdb3b3308e8b01c5a067d31bcff21bff
kplv <denuc.kopylov@gmail.com>
2024-10-10 18:38:36 +0200

    TR new block and image

--- .next/static/webpack/webpack.d7d8fd73808de112.hot-update.js
+++ .next/static/webpack/webpack.d7d8fd73808de112.hot-update.js
@@ -9,9 +9,14 @@
  */
 self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/amd options */
+/******/ !function() {
+/******/ 	__webpack_require__.amdO = {};
+/******/ }();
+/******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "f1d897ea0afc9fb1"; }
+/******/ 	__webpack_require__.h = function() { return "8767926311a42a58"; }
 /******/ }();
 /******/ 
 /******/ }
